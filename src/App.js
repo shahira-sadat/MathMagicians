2b8fc356d2106d084f347817c8a157b89dbb8991
shahira-sadat <shahira.sadat1@gmail.com>
2022-08-02 20:01:21 +0430

    Add class based component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React, { Component } from 'react';
 import Calculator from './components/Calculator';
 
-function App() {
-  return <Calculator />;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
 
 export default App;
