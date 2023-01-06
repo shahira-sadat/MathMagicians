6e4b747557a0cf9b402d133eabbbdc09a6e5daac
shahira-sadat <shahira.sadat1@gmail.com>
2023-01-06 16:51:30 +0430

    Update content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,16 @@
 import './Home.css';
 
-const CONTENT = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-Suspendisse at enim viverra, volutpat velit vitae, pretium sem. 
-Nullam laoreet dui eget tellus aliquam, a semper est dignissim. 
-Quisque iaculis enim malesuada lacus bibendum pulvinar. 
-Phasellus quis tortor in dui placerat aliquam. a semper est dignissim. 
-Quisque iaculis enim malesuada lacus bibendum pulvinar. 
-Phasellus quis tortor in dui placerat aliquam.
+const CONTENT = `Calculators have not only greatly enhanced our ability to perform the regular computations
+ that are involved in everyday life, but provided humans with the ability to understand mathematics on a 
+ greater scale than ever imagined.
+ This technology allows students solve complicated problems quickly and in an efficient manner Students 
+ can develop fluency and mental math skills without a calculator.
 `;
 
 function Home() {
   return (
     <section className="home">
       <h2 className="section-title">Welcome to our page!</h2>
-      <p className="content">{CONTENT}</p>
       <p className="content">{CONTENT}</p>
     </section>
   );
